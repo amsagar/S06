077ca014a76e8acdaadd9677a09ca6418f226cef
SAGAR PATIL <am.sagarpatil@gmail.com>
2023-04-26 06:43:23 +0530

    app

--- VoltageVikingsApp/apps.py
+++ VoltageVikingsApp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VoltagevikingsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'VoltageVikingsApp'
+
+    def ready(self):
+        import VoltageVikingsApp.signals
